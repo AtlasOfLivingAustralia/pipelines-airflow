e6e4bb01906c3da82663a9299104bd3805826b92
Mahmoud <sadeghim@gmail.com>
2024-12-02 19:29:19 +1100

    Added success notification operator as last task to DAGs

--- dags/create_dag.py
+++ dags/create_dag.py
@@ -10,18 +10,18 @@ s3_job_config_path = ala_config.JOB_SCHEDULE_CONFIG
 
 @dataclass
 class CONFIG_KEY:
-    REGISTRY_UID = 'datasetId'
-    TAG_LABEL = 'tags'
-    JOB_NAME = 'job_name'
-    DESCRIPTION = 'description'
-    SCHEDULE = 'schedule'
-    TRIGGER_DAG_ID = 'trigger_dag_id'
-    DAG_PARAMS = 'params'
-    PREINGEST_TRIGGER_DAG_ID = 'Preingest_datasets'
-    PREINGEST_DATASET_ID = 'datasetId'
-    PREINGEST_EXTRA_ARGS = 'extra_args'
-    PREINGEST_INSTANCE_TYPE = 'instanceType'
-    PREINGEST_LOAD_IMAGES = 'load_images'
+    REGISTRY_UID = "datasetId"
+    TAG_LABEL = "tags"
+    JOB_NAME = "job_name"
+    DESCRIPTION = "description"
+    SCHEDULE = "schedule"
+    TRIGGER_DAG_ID = "trigger_dag_id"
+    DAG_PARAMS = "params"
+    PREINGEST_TRIGGER_DAG_ID = "Preingest_datasets"
+    PREINGEST_DATASET_ID = "datasetId"
+    PREINGEST_EXTRA_ARGS = "extra_args"
+    PREINGEST_INSTANCE_TYPE = "instanceType"
+    PREINGEST_LOAD_IMAGES = "load_images"
 
 
 class CreateDagOperator:
@@ -33,10 +33,10 @@ class CreateDagOperator:
 
         if s3_path:
             (bucket_name, path) = s3_path.replace("s3://", "").split("/", 1)
-            s3_resource = boto3.resource('s3')
+            s3_resource = boto3.resource("s3")
             try:
                 config_job = s3_resource.Object(bucket_name, path)
-                return json.load(config_job.get()['Body'])
+                return json.load(config_job.get()["Body"])
             except Exception as e:
                 print(f"Failed to load config file from {s3_path} with error {e}")
                 return {}
@@ -44,8 +44,14 @@ class CreateDagOperator:
             return {}
 
     @staticmethod
-    def create_dag(dag_id: str, trigger_dag_id: str, description: str, schedule: str, tags: [], params: {}):
-
+    def create_dag(
+        dag_id: str,
+        trigger_dag_id: str,
+        description: str,
+        schedule: str,
+        tags: [],
+        params: {},
+    ):
         """
         Creates a scheduled wrapper dag. Schedule dag calls either preingest data resource or preingest provider dag.
         There is current bug in airflow 2.2.2 whereby upon deploying a new scheduled dag, the dag gets triggered twice.
@@ -80,9 +86,17 @@ class CreateDagOperator:
         catchup = False
         start_date = get_start_date(schedule, catchup)
 
-        dag = DAG(dag_id=dag_id, description=description, start_date=start_date, schedule_interval=schedule,
-                  default_args=ala_helper.get_default_args(), catchup=catchup, tags=tags, is_paused_upon_creation=True,
-                  max_active_runs=1)
+        dag = DAG(
+            dag_id=dag_id,
+            description=description,
+            start_date=start_date,
+            schedule_interval=schedule,
+            default_args=ala_helper.get_default_args(),
+            catchup=catchup,
+            tags=tags,
+            is_paused_upon_creation=True,
+            max_active_runs=1,
+        )
 
         with dag:
             triggered_task = TriggerDagRunOperator(
@@ -91,10 +105,10 @@ class CreateDagOperator:
                 trigger_dag_id=trigger_dag_id,
                 wait_for_completion=True,
                 trigger_rule=TriggerRule.NONE_SKIPPED,
-                conf=params
+                conf=params,
             )
 
-            triggered_task
+            triggered_task >> ala_helper.get_success_notification_operator()
 
         return dag
 
@@ -116,23 +130,44 @@ for job in job_list:
         trigger_dag_id = job[CONFIG_KEY.TRIGGER_DAG_ID]
         params = job[CONFIG_KEY.DAG_PARAMS]
         # Handle tuple for (None, None)
-        params = {key: (None, None) if value == "(None, None)" else value for key, value in params.items()}
+        params = {
+            key: (None, None) if value == "(None, None)" else value
+            for key, value in params.items()
+        }
     else:
         trigger_dag_id = CONFIG_KEY.PREINGEST_TRIGGER_DAG_ID
         registry_entity_uid = job[CONFIG_KEY.PREINGEST_DATASET_ID]
-        instanceType = job[CONFIG_KEY.PREINGEST_INSTANCE_TYPE] if 'instanceType' in job else 'm6g.xlarge'
+        instanceType = (
+            job[CONFIG_KEY.PREINGEST_INSTANCE_TYPE]
+            if "instanceType" in job
+            else "m6g.xlarge"
+        )
         if not any(registry_entity_uid in ele for ele in tags):
             tags.insert(0, registry_entity_uid)
         tags.append(instanceType)
-        extra_args = job[CONFIG_KEY.PREINGEST_EXTRA_ARGS] if CONFIG_KEY.PREINGEST_EXTRA_ARGS in job else {}
-        load_images = ala_config.LOAD_IMAGES if CONFIG_KEY.PREINGEST_LOAD_IMAGES not in job else job[
-            CONFIG_KEY.PREINGEST_LOAD_IMAGES]
-        params = {"datasetIds": registry_entity_uid, CONFIG_KEY.PREINGEST_LOAD_IMAGES: load_images,
-                  "instanceType": instanceType, "extra_args": extra_args, "override_uuid_percentage_check": "false"}
+        extra_args = (
+            job[CONFIG_KEY.PREINGEST_EXTRA_ARGS]
+            if CONFIG_KEY.PREINGEST_EXTRA_ARGS in job
+            else {}
+        )
+        load_images = (
+            ala_config.LOAD_IMAGES
+            if CONFIG_KEY.PREINGEST_LOAD_IMAGES not in job
+            else job[CONFIG_KEY.PREINGEST_LOAD_IMAGES]
+        )
+        params = {
+            "datasetIds": registry_entity_uid,
+            CONFIG_KEY.PREINGEST_LOAD_IMAGES: load_images,
+            "instanceType": instanceType,
+            "extra_args": extra_args,
+            "override_uuid_percentage_check": "false",
+        }
 
-    globals()[dag_id] = CreateDagOperator.create_dag(dag_id=dag_id,
-                                                     trigger_dag_id=trigger_dag_id,
-                                                     description=description,
-                                                     schedule=schedule,
-                                                     tags=tags,
-                                                     params=params)
+    globals()[dag_id] = CreateDagOperator.create_dag(
+        dag_id=dag_id,
+        trigger_dag_id=trigger_dag_id,
+        description=description,
+        schedule=schedule,
+        tags=tags,
+        params=params,
+    )
