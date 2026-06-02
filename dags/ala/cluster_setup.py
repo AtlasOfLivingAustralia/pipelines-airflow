9cf27605124eaa8b9632e7fc5368482bde5e95f9
Patricia Koh <patricia.koh@csiro.au>
2026-06-03 09:51:49 +1000

    https://github.com/AtlasOfLivingAustralia/pipelines-airflow/issues/37 - Remove AMI reference when setting up preingestion cluster

--- dags/ala/cluster_setup.py
+++ dags/ala/cluster_setup.py
@@ -268,7 +268,7 @@ class PreIngestionEMRConfig(EMRConfig):
                 "Market": ala_config.MASTER_MARKET,
                 "InstanceRole": "MASTER",
                 "InstanceType": self.instance_type,
-                "CustomAmiId": ala_config.PREINGESTION_AMI,
+                # "CustomAmiId": ala_config.PREINGESTION_AMI,
                 "InstanceCount": 1,
                 "EbsConfiguration": {
                     "EbsBlockDeviceConfigs": [
