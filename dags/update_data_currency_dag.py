a5f775d0dfaf0722e8d4712197eeea53a6852b96
Christopher Mangion <christopher.mangion@hotmail.com>
2026-08-28 15:58:31 +1000

    Fixed incorrect decorator import location Number of buckets found in solr now reported in get_solr_load_dates step Updated the wording of some logging outputs

--- dags/update_data_currency_dag.py
+++ dags/update_data_currency_dag.py
@@ -1,7 +1,7 @@
 import logging as log
 from datetime import timedelta
 import requests
-from airflow.sdk import dag, task
+from airflow.decorators import dag, task
 from airflow.exceptions import AirflowException
 from ala.ala_helper import get_default_args, update_registry_metadata
 from ala import ala_config
@@ -16,7 +16,7 @@ from ala import ala_config
 )
 def update_data_currency_values(datasetIDs: str = ""):
     """
-    Updates dataCurrency field in collectory from the max lastLoadDate value for the respective data resource in solr
+    Updates dataCurrency field in collectory from the max lastLoadDate value for the respective data resource in solr  
     Passing no datasetIDs gets every available data resource in solr and updates collectory
 
     Defaults to all data resources
@@ -25,9 +25,11 @@ def update_data_currency_values(datasetIDs: str = ""):
     solr_facet = "lastLoadDate"
     collectory_value = "dataCurrency"
 
-    @task(multiple_outputs=True)
+    @task(multiple_outputs=True, show_return_value_in_logs=False)
     def get_solr_load_dates(datasetIDs: str) -> dict:
-        filter_uid_list = [] if datasetIDs == "None" else datasetIDs.split() # Empty str passed in as "None"
+        filter_uid_list = datasetIDs.split()
+
+        solr_bucket_name = "distinct_items"
 
         url = f"{ala_config.SOLR_URL}/biocache/select"
         headers = {
@@ -38,7 +40,7 @@ def update_data_currency_values(datasetIDs: str = ""):
             "query": "*:*",
             "limit": 0,
             "facet": {
-                "distinct_items": {
+                solr_bucket_name: {
                     "type": "terms",
                     "field": "dataResourceUid",
                     "limit": -1,
@@ -59,9 +61,12 @@ def update_data_currency_values(datasetIDs: str = ""):
         if response.status_code != 200:
             raise AirflowException(f"Got error from solr({response.status_code} - {response.reason}): {payload['error']['msg']}")
 
+        payload = payload["facets"][solr_bucket_name] # Overwrite initial payload with just the returned bucket
+        log.info(f"Found {payload['numBuckets']} data resources in solr with facet '{solr_facet}'")
+
         # Get required updates from payload
         updates = {}
-        for bucket in payload["facets"]["distinct_items"]["buckets"]:
+        for bucket in payload["buckets"]:
             dr_uid = bucket["val"]
 
             if filter_uid_list and (dr_uid not in filter_uid_list):
@@ -84,8 +89,9 @@ def update_data_currency_values(datasetIDs: str = ""):
 
         for dr_uid, value in updates.items():
             if value is None: # dr_uid not returned from solr
-                log.error(f"Error updating {dr_uid}, no data resource found in solr")
+                log.error(f"Error updating {dr_uid}, data resource not found in solr")
                 solr_errors.append(dr_uid)
+                continue
 
             if not value: # Value from solr was empty
                 log.warning(f"Skipping {collectory_value} update for {dr_uid}, solr value is empty")
@@ -104,13 +110,11 @@ def update_data_currency_values(datasetIDs: str = ""):
             return '' if not item_list else (": " + ", ".join(item_list))
 
         failed = sum(len(errors) for errors in [solr_errors, value_errors, collectory_errors])
-        success = len(updates) - failed
 
-        log.info(f"Found {len(updates)} matching data resource(s) in solr with '{solr_facet}'")
-        log.info(f"Successfully updated '{collectory_value}' for {success} in collectory")
-        log.info(f"Failed to update {len(value_errors)} data resource(s) with no solr value{_item_str(value_errors)}")
-        log.info(f"Failed to update {len(collectory_errors)} data resource(s) with collectory issue{_item_str(collectory_errors)}")
+        log.info(f"Successfully updated '{collectory_value}' for {len(updates) - failed} data resource(s) in collectory")
         log.info(f"Failed to update {len(solr_errors)} data resource(s) as dr doesn't exist in solr{_item_str(solr_errors)}")
+        log.info(f"Failed to update {len(value_errors)} data resource(s) with no solr value{_item_str(value_errors)}")
+        log.info(f"Failed to update {len(collectory_errors)} data resource(s) due to collectory issue{_item_str(collectory_errors)}")
 
     updates = get_solr_load_dates(datasetIDs)
     update_collectory_data_currency(updates)
