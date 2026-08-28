4cdeadccd36d0f4f0924b2c264fb07c293ca8743
Christopher Mangion <christopher.mangion@hotmail.com>
2026-08-28 10:14:30 +1000

    Updating data currency now accepts data resource IDs as a filter for values to update only Uncommented the line that actually updates collectory metadata

--- dags/update_data_currency_dag.py
+++ dags/update_data_currency_dag.py
@@ -19,10 +19,14 @@ with DAG(
     start_date=days_ago(1),
     schedule_interval=None,
     tags=["emr", "all-datasets"],
-    params={}
+    params={
+        "datasetIds": ""
+    }
 ) as dag:
     
-    def update_dates_from_solr() -> None:
+    def update_dates_from_solr(datasetIDs: str) -> None:
+        filter_uid_list = datasetIDs.split()
+
         solr_facet = "lastLoadDate"
         collectory_value = "dataCurrency"
 
@@ -59,6 +63,9 @@ with DAG(
         for bucket in distinct_items["buckets"]:
             dr_uid = bucket["val"]
 
+            if filter_uid_list and dr_uid not in filter_uid_list:
+                continue
+
             value = bucket.get(solr_facet, "")
             if not value:
                 log.warning(f"Skipping {collectory_value} update for dr {dr_uid}, solr value is empty")
@@ -66,23 +73,31 @@ with DAG(
                 continue
 
             try:
-                # update_registry_metadata(dr_uid, {collectory_value: value})
+                update_registry_metadata(dr_uid, {collectory_value: value})
                 log.info(f"Updated {collectory_value} for dr {dr_uid} to: {value}")
             except Exception as e:
                 log.error(f"Error updating {collectory_value} for dr {dr_uid}: {e}")
                 collectory_errors.append(dr_uid)
 
-        def _print_errors(error_uids: list[str]) -> str:
-            return '' if not error_uids else (": " + ", ".join(error_uids))
+        def _item_str(item_list: list[str]) -> str:
+            return '' if not item_list else (": " + ", ".join(item_list))
+
+        def _dr_count_str(count: int) -> str:
+            return f"{count} data resource{'s' if count > 1 else ''}"
 
         total_buckets = distinct_items["numBuckets"]
+        selected_buckets = total_buckets if not filter_uid_list else len(filter_uid_list)
         total_failed = len(solr_errors) + len(collectory_errors)
-        log.info(f"Total data resources with '{solr_facet}' in solr: {total_buckets}")
-        log.info(f"Succecssfully updated '{collectory_value}' for {total_buckets - total_failed} data resources in collectory")
-        log.info(f"Failed to update {len(solr_errors)} data resoures with no solr value{_print_errors(solr_errors)}")
-        log.info(f"Failed to update {len(collectory_errors)} data resources with collectory issue{_print_errors(collectory_errors)}")
+
+        log.info(f"Found {_dr_count_str(total_buckets)} in solr with '{solr_facet}'")
+        if filter_uid_list:
+            log.info(f"Update attempted on subset of {_dr_count_str(len(filter_uid_list))}")
+        log.info(f"Succecssfully updated '{collectory_value}' for {_dr_count_str(selected_buckets - total_failed)} in collectory")
+        log.info(f"Failed to update {_dr_count_str(len(solr_errors))} with no solr value{_item_str(solr_errors)}")
+        log.info(f"Failed to update {_dr_count_str(len(collectory_errors))} with collectory issue{_item_str(collectory_errors)}")
 
     update_data_currency_values = PythonOperator(
         task_id="update_date_currency_dates",
-        python_callable=update_dates_from_solr
+        python_callable=update_dates_from_solr,
+        op_args=["{{ params.datasetIds }}"]
     )
