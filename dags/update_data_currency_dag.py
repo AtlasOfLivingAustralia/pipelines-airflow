d1ef5b1e4aef49906e6d585bd23bcdf264665401
Christopher Mangion <christopher.mangion@hotmail.com>
2026-08-28 11:22:20 +1000

    Now generate updates dict to catch issues with provided dr uids not returning from solr No longer generate dicts of subsections of payload Fixed issue with passing empty datasetIDs not updating any Updated logging to better reflect outputs Removed imports that are no longer required

--- dags/update_data_currency_dag.py
+++ dags/update_data_currency_dag.py
@@ -1,6 +1,5 @@
 import logging as log
-from datetime import datetime, timedelta
-import boto3
+from datetime import timedelta
 import requests
 from airflow import DAG
 from airflow.utils.dates import days_ago
@@ -25,7 +24,7 @@ with DAG(
 ) as dag:
     
     def update_dates_from_solr(datasetIDs: str) -> None:
-        filter_uid_list = datasetIDs.split()
+        filter_uid_list = [] if datasetIDs == "None" else datasetIDs.split() # Empty str passed in as "None"
 
         solr_facet = "lastLoadDate"
         collectory_value = "dataCurrency"
@@ -57,44 +56,53 @@ with DAG(
             log.info(f"Got {response.status_code} from solr: {response.reason}")
             AirflowException(f"Got error from solr: {payload['error']['msg']}")
 
-        solr_errors = []
-        collectory_errors = []
-        distinct_items = payload["facets"]["distinct_items"]
-        for bucket in distinct_items["buckets"]:
+        # Get required updates from payload
+        updates = {}
+        for bucket in payload["facets"]["distinct_items"]["buckets"]:
             dr_uid = bucket["val"]
 
-            if filter_uid_list and dr_uid not in filter_uid_list:
+            if filter_uid_list and (dr_uid not in filter_uid_list):
                 continue
 
-            value = bucket.get(solr_facet, "")
+            updates[dr_uid] = bucket.get(solr_facet, "")
+
+        # Apply updates and collect error uids
+        value_errors = []
+        collectory_errors = []
+        for dr_uid, value in updates.items():
             if not value:
-                log.warning(f"Skipping {collectory_value} update for dr {dr_uid}, solr value is empty")
-                solr_errors.append(dr_uid)
+                log.warning(f"Skipping {collectory_value} update for {dr_uid}, solr value is empty")
+                value_errors.append(dr_uid)
                 continue
 
             try:
                 update_registry_metadata(dr_uid, {collectory_value: value})
-                log.info(f"Updated {collectory_value} for dr {dr_uid} to: {value}")
+                log.info(f"Updated '{collectory_value}' for {dr_uid}: {value}")
             except Exception as e:
-                log.error(f"Error updating {collectory_value} for dr {dr_uid}: {e}")
+                log.error(f"Error updating {collectory_value} for {dr_uid}: {e}")
                 collectory_errors.append(dr_uid)
 
+        solr_errors = []
+        for dr_uid in filter_uid_list:
+            if dr_uid not in updates:
+                log.error(f"Error updating {dr_uid}, no data resource found in solr")
+                solr_errors.append(dr_uid)
+
+        # Log activity
         def _item_str(item_list: list[str]) -> str:
             return '' if not item_list else (": " + ", ".join(item_list))
 
         def _dr_count_str(count: int) -> str:
-            return f"{count} data resource{'s' if count > 1 else ''}"
+            return f"{count} data resource{'s' if count != 1 else ''}"
 
-        total_buckets = distinct_items["numBuckets"]
-        selected_buckets = total_buckets if not filter_uid_list else len(filter_uid_list)
-        total_failed = len(solr_errors) + len(collectory_errors)
+        selected_buckets = len(updates)
+        success = selected_buckets - (len(value_errors) + len(collectory_errors)) # Solr errors not in updates, dont subtract
 
-        log.info(f"Found {_dr_count_str(total_buckets)} in solr with '{solr_facet}'")
-        if filter_uid_list:
-            log.info(f"Update attempted on subset of {_dr_count_str(len(filter_uid_list))}")
-        log.info(f"Succecssfully updated '{collectory_value}' for {_dr_count_str(selected_buckets - total_failed)} in collectory")
-        log.info(f"Failed to update {_dr_count_str(len(solr_errors))} with no solr value{_item_str(solr_errors)}")
+        log.info(f"Found {_dr_count_str(selected_buckets)} in solr with '{solr_facet}'")
+        log.info(f"Succecssfully updated '{collectory_value}' for {_dr_count_str(success)} in collectory")
+        log.info(f"Failed to update {_dr_count_str(len(value_errors))} with no solr value{_item_str(value_errors)}")
         log.info(f"Failed to update {_dr_count_str(len(collectory_errors))} with collectory issue{_item_str(collectory_errors)}")
+        log.info(f"Failed to update {_dr_count_str(len(solr_errors))} as dr doesn't exist in solr{_item_str(solr_errors)}")
 
     update_data_currency_values = PythonOperator(
         task_id="update_date_currency_dates",
